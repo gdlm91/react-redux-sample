3d86a8fec32ac8d74e73e3e033dc764d561a45b3
Gerardo Leal <gleal@atlassian.com>
2021-01-11 23:15:25 +1100

    Example with subscription from the store

--- src/components/Albums.tsx
+++ src/components/Albums.tsx
@@ -1,10 +1,23 @@
 import * as React from "react";
 import { useDispatch } from "react-redux";
 
+import store from "../redux";
 import { addAlbum, removeLastAlbum } from "../redux/actions/albums";
+import { Album } from "../types";
 
 const Albums: React.FunctionComponent = () => {
   const dispatch = useDispatch();
+  const [albums, setAlbums] = React.useState<Album[]>([]);
+
+  React.useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      const { albums: _albums } = store.getState();
+
+      setAlbums(_albums);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const handleAddAlbum = () => {
     dispatch(
@@ -23,6 +36,12 @@ const Albums: React.FunctionComponent = () => {
   return (
     <div>
       <h2>Albums</h2>
+
+      <ul>
+        {albums.map((album) => (
+          <li>{album.title}</li>
+        ))}
+      </ul>
 
       <button onClick={handleAddAlbum}>Add album</button>
       <button onClick={handleRemoveLastAlbum}>Remove last album</button>
